9b411352eadb8d81dddc1492099d0887a29f7049
F64051114 <f64051114@mail.ncku.edu.tw>
2018-03-16 20:39:27 +0800

    I do it through do while method.

---
+++ A.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+	cout<<n<<" ";
+	do
+	{
+		if(n%2!=0) //n is odd
+		{
+			n=3*n+1;
+			cout<<n<<" ";
+		}
+		else
+		{
+			n=n/2;
+			cout<<n<<" ";
+		}
+	}while(n!=1);
+	cout<<"\n";
+	return 0;
+}
